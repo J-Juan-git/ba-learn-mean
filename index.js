560a6e334cdecbb6753fe5586c68898495cd6aed
J-Juan-git <joshuajuan0010@gmail.com>
2021-05-04 21:52:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express()
+const port = process.env.PORT;
+const path = __dirname
+app.use(express.static(__dirname+'/frontend'))
+app.get('/',(req,res)=>{
+    res.send("Home Page");
+})
+app.get('/google-home-page',(req,res)=>{
+    res.sendFile(path+"/frontend/html/google.html");
+})
+app.listen(port || 5000, ()=>{
+    console.log("Server is up and running");
+})
