f348c05581cddeaab429b77fad6b0e2b31200482
J-Juan-git <joshuajuan0010@gmail.com>
2021-05-13 13:47:40 +0530

    Added home Page

---
+++ frontend/js/index.js
@@ -0,0 +1,65 @@
+const about = document.querySelector('#about')
+const projects = document.querySelector('#projects')
+const projectContent = document.querySelector("#project-content")
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    // modal: true,
+    width: '400px',
+    height: '400px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#000')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    background: '#018786',    
+    width: '400px',
+    height: '400px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+projects.addEventListener('click', () => {
+    const projectBox = new WinBox({
+      title: 'My Projects',
+      background: '#018786',    
+      width: '400px',
+      height: '400px',
+      top: 150,
+      right: 50,
+      bottom: 50,
+      left: 250,
+      mount: projectContent,
+      onfocus: function () {
+        this.setBackground('#00aa00')
+      },
+      onblur: function () {
+        this.setBackground('#777')
+      },
+    })
+  })
